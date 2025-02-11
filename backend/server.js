6eff5c8894150abe5f6b6a6b7268142d5f0e807c
Aleks <karaeeeani@gmail.com>
2025-02-11 10:56:15 +0200

    Rate limiting and bot protection code + update Readme

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import dotenv from "dotenv";
 import productRoutes from "./routes/productRoutes.js";
 
 import { sql } from "./config/db.js";
+import { aj } from "./lib/arcjet.js";
 
 dotenv.config();
 
@@ -20,6 +21,37 @@ app.use(cors());
 app.use(helmet());
 
 app.use(morgan("dev"));
+
+app.use(async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req, {
+      requested: 1,
+    });
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        res.status(429).json({ error: "Too many requests" });
+      } else if (decision.reason.isBot()) {
+        res.status(403).json({ error: "Bot access denied" });
+      } else {
+        res.status(403).json({ error: "Forbidden" });
+      }
+      return;
+    }
+    if (
+      decision.results.some(
+        (result) => result.reason.isBot() && result.reason.isSpoofed()
+      )
+    ) {
+      res.status(403).json({ error: "Spoofed bot detected" });
+      return;
+    }
+    next();
+  } catch (error) {
+    console.log("Arcjet Error", error);
+    next(error);
+  }
+});
 
 app.use("/api/products", productRoutes);
 
